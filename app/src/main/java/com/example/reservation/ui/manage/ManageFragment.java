3f997442e60ac4464e8f7c6ddd0e125b95c3a6c1
mimi <alal39603819@gmail.com>
2019-11-28 04:34:11 +0900

    현재 운영시간텍스트클릭시 타임피커나오게

--- app/src/main/java/com/example/reservation/ui/manage/ManageFragment.java
+++ app/src/main/java/com/example/reservation/ui/manage/ManageFragment.java
@@ -1,5 +1,8 @@
 package com.example.reservation.ui.manage;
 
+import android.annotation.SuppressLint;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -37,17 +40,14 @@ public class ManageFragment extends Fragment {
     private ManageViewModel shareViewModel;
     private String restaurant1 = "";
     private String open_hour="";
-    private String open_minute="";
     private String close_hour="";
-    private String close_minute="";
-    EditText editrestaurant_name;
-    TimePicker open_tp, close_tp;
-    int openhour, openminute,closehour, closeminute;
-    private static final int TIME_PICKER_INTERVAL=30;
+    EditText edit_close,edit_open;
+    TextView editrestaurant_name;
+    String open,close;
     Button editcheck;
-
+    TimePickerDialog timePickerDialog;
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+                             final ViewGroup container, Bundle savedInstanceState) {
         context=container.getContext();
         shareViewModel =
                 ViewModelProviders.of(this).get(ManageViewModel.class);
@@ -60,7 +60,6 @@ public class ManageFragment extends Fragment {
             }
         });
 
-
         FirebaseDatabase database1 = FirebaseDatabase.getInstance();
         final DatabaseReference myRef1 = database1.getReference("User_info/");
 
@@ -72,34 +71,54 @@ public class ManageFragment extends Fragment {
         final String id1 = intent.getExtras().getString("id");
         final String key1 = intent.getExtras().getString("key");
 
-        editrestaurant_name = (EditText) root.findViewById(R.id.restaurant_name);
-        open_tp=(TimePicker)root.findViewById(R.id.timePicker_open);
-        close_tp=(TimePicker)root.findViewById(R.id.timePicker_close);
+        editrestaurant_name = (TextView) root.findViewById(R.id.restaurant_name);
+        edit_close=(EditText)root.findViewById(R.id.edit_close);
+        edit_open=(EditText)root.findViewById(R.id.edit_open);
         editcheck = (Button) root.findViewById(R.id.check);
-        setTimePickerInterval(open_tp);
-        setTimePickerInterval(close_tp);
+
 
         myRef1.addListenerForSingleValueEvent(new ValueEventListener() {//id값으로 정보를 찾음
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 getMyRestaurantManage(dataSnapshot, id1);
                 editrestaurant_name.setText(restaurant1);
-                openhour = open_tp.getHour();
-                openminute=open_tp.getMinute();
-                closehour=close_tp.getHour();
-                closeminute=close_tp.getMinute();
+                edit_open.setText(open);//현재 오픈시간
+                edit_close.setText(close);//현재 마감시간
 
-                editcheck.setOnClickListener(new View.OnClickListener() {
+                edit_open.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        myRef1.child(key1).child("open_hour").setValue(String.valueOf(openhour));//시간 변경
-                        myRef1.child(key1).child("open_minute").setValue(String.valueOf(openminute*30));
-                        myRef1.child(key1).child("close_hour").setValue(String.valueOf(closehour));
-                        myRef1.child(key1).child("close_minute").setValue(String.valueOf(closeminute*30));
-                        Toast.makeText(context, "운영시간이 변경되었습니다.", Toast.LENGTH_LONG).show();
+                        timePickerDialog = new TimePickerDialog(context, new OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                edit_open.setText(hourOfDay+"시 "+minute+"분");//새 오픈시간
+
+                            }
+                        }, 15,24, false);
+                        timePickerDialog.show();
+                    }
+                });
+                edit_close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        timePickerDialog = new TimePickerDialog(context, new OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                edit_close.setText(hourOfDay+"시 "+minute+"분");//새 마감시간
+                            }
+                        }, 15,24, false);
+                        timePickerDialog.show();
                     }
                 });
 
+             editcheck.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    myRef1.child(key1).child("open").setValue(String.valueOf(edit_open.getText().toString()));//시간 변경
+                    myRef1.child(key1).child("close").setValue(String.valueOf(edit_close.getText().toString()));
+                    Toast.makeText(context, "운영시간이 변경되었습니다.", Toast.LENGTH_LONG).show();
+                }
+            });
 
 
             }
@@ -108,50 +127,28 @@ public class ManageFragment extends Fragment {
             }
         });
 
-
-
-
-
-
         return root;
 
     }
-
-
+    //기존의 값들을 받아옴
     private void getMyRestaurantManage(@NonNull DataSnapshot dataSnapshot, String myId) {
         for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
             User user_each = childSnapshot.getValue(User.class);
-            if (user_each.getId1().equals(myId)) {//intent로 받은 값이랑 반복문을 통해서 확인한 아이디 값이랑 같으면
-                restaurant1 = user_each.getRestaurant_name();//어플 사용하는 사장님의 가게 이름이다
-                open_hour=user_each.getOpen_hour();
-                open_minute=user_each.getOpen_minute();
-                close_hour=user_each.getClose_hour();
-                close_minute=user_each.getClose_minute();
+            if (user_each.getId1().equals(myId)) {
+                restaurant1 = user_each.getRestaurant_name();
+                open=user_each.getOpen();
+                close=user_each.getClose();
+
                 Log.i("가게이름", restaurant1);
                 Log.i("manage_open",open_hour);
                 Log.i("manage_close",close_hour);
-                //Toast.makeText(getContext(), id1+"\n"+key1+"\nrestaurant1: "+restaurant1,Toast.LENGTH_LONG).show();
+
                 break;
             } else {
                 continue;
             }
         }
     }
-    //타임피커 30분단위 설정
-    private void setTimePickerInterval(TimePicker timePicker) {
-        try {
-            NumberPicker minutePicker = (NumberPicker) timePicker.findViewById(Resources.getSystem().getIdentifier(
-                    "minute", "id", "android"));
-            minutePicker.setMinValue(0);
-            minutePicker.setMaxValue((60/ TIME_PICKER_INTERVAL) - 1);
-            List<String> displayedValues = new ArrayList<String>();
-            for (int i = 0; i < 60; i += TIME_PICKER_INTERVAL) {
-                displayedValues.add(String.format("%02d", i));
-            }
-            minutePicker.setDisplayedValues(displayedValues.toArray(new String[0]));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+
 
 }
