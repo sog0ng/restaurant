75e9160c63da6cb294ca5803423f4c18bc3070de
mimi <alal39603819@gmail.com>
2019-11-28 01:04:12 +0900

    infofragment 조건추ㅏㄱ

--- app/src/main/java/com/example/reservation/ui/info/InfoFragment.java
+++ app/src/main/java/com/example/reservation/ui/info/InfoFragment.java
@@ -20,12 +20,16 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.reservation.R;
+import com.example.reservation.SignUpActivity;
 import com.example.reservation.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InfoFragment extends Fragment {
     private Context context;
@@ -78,7 +82,7 @@ public class InfoFragment extends Fragment {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 getMyRestaurantInfo(dataSnapshot, id1);
-                if(is_owner.equals("0")){
+                if(is_owner.equals("0")){//사장일 경우
                     editrestaurant_name.setVisibility(View.VISIBLE);
                     editid1.setHint(id1);
                     editpw.setText(password);
@@ -90,6 +94,45 @@ public class InfoFragment extends Fragment {
                     editsubmit.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
+                            // 비밀번호 입력 확인
+                            if (editpw.getText().toString().length() == 0) {
+                                Toast.makeText(context, "비밀번호를 입력하세요!", Toast.LENGTH_SHORT).show();
+                                editpw.requestFocus();
+                                return;
+                            }
+
+                            // 비밀번호 확인 입력 확인
+                            if (editpassword_check.getText().toString().length() == 0) {
+                                Toast.makeText(context, "비밀번호 확인을 입력하세요!", Toast.LENGTH_SHORT).show();
+                                editpassword_check.requestFocus();
+                                return;
+                            }
+
+                            // 비밀번호 일치 확인
+                            if (!editpw.getText().toString().equals(editpassword_check.getText().toString())) {
+                                Toast.makeText(context, "비밀번호가 일치하지 않습니다!", Toast.LENGTH_SHORT).show();
+                                editpw.setText("");
+                                editpassword_check.setText("");
+                                editpw.requestFocus();
+                                return;
+                            }
+
+                            // 비밀번호 조건 확인
+                            if (!ConfirmPassword(editpw.getText().toString())) {
+                                Toast.makeText(context, "8~16자 길이로 영어 대소문자, 숫자를 혼합하세요!", Toast.LENGTH_SHORT).show();
+                                editpw.setText("");
+                                editpassword_check.setText("");
+                                editpw.requestFocus();
+                                return;
+                            }
+                            // 전화 번호 조건 확인
+                            if( !ConfirmPhonenum(editphone_num.getText().toString()) )
+                            {
+                                Toast.makeText(context,"올바른 번호를 입력하세요!",Toast.LENGTH_SHORT).show();
+                                editphone_num.setText("");
+                                editphone_num.requestFocus();
+                                return;
+                            }
                             myRef1.child(key1).child("password").setValue(editpw.getText().toString());//비밀번호 변경
                             myRef1.child(key1).child("phone_num").setValue(editphone_num.getText().toString());//번호 변경
                             Toast.makeText(context, "변경사항이 수정되었습니다.", Toast.LENGTH_LONG).show();
@@ -98,7 +141,7 @@ public class InfoFragment extends Fragment {
                         }
                     });
                 }
-                else{
+                else{//고객일경우
                     editrestaurant_name.setVisibility(View.GONE);
                     textrestaurant_name.setVisibility(View.GONE);
 
@@ -111,6 +154,44 @@ public class InfoFragment extends Fragment {
                     editsubmit.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
+                            // 비밀번호 입력 확인
+                            if (editpw.getText().toString().length() == 0) {
+                                Toast.makeText(context, "비밀번호를 입력하세요!", Toast.LENGTH_SHORT).show();
+                                editpw.requestFocus();
+                                return;
+                            }
+
+                            // 비밀번호 확인 입력 확인
+                            if (editpassword_check.getText().toString().length() == 0) {
+                                Toast.makeText(context, "비밀번호 확인을 입력하세요!", Toast.LENGTH_SHORT).show();
+                                editpassword_check.requestFocus();
+                                return;
+                            }
+
+                            // 비밀번호 일치 확인
+                            if (!editpw.getText().toString().equals(editpassword_check.getText().toString())) {
+                                Toast.makeText(context, "비밀번호가 일치하지 않습니다!", Toast.LENGTH_SHORT).show();
+                                editpw.setText("");
+                                editpassword_check.setText("");
+                                editpw.requestFocus();
+                                return;
+                            }
+                            // 비밀번호 조건 확인
+                            if (!ConfirmPassword(editpw.getText().toString())) {
+                                Toast.makeText(context, "8~16자 길이로 영어 대소문자, 숫자를 혼합하세요!", Toast.LENGTH_SHORT).show();
+                                editpw.setText("");
+                                editpassword_check.setText("");
+                                editpw.requestFocus();
+                                return;
+                            }
+                            // 전화 번호 조건 확인
+                            if( !ConfirmPhonenum(editphone_num.getText().toString()) )
+                            {
+                                Toast.makeText(context,"올바른 번호를 입력하세요!",Toast.LENGTH_SHORT).show();
+                                editphone_num.setText("");
+                                editphone_num.requestFocus();
+                                return;
+                            }
                             myRef1.child(key1).child("password").setValue(editpw.getText().toString());//비밀번호 변경
                             myRef1.child(key1).child("phone_num").setValue(editphone_num.getText().toString());//번호 변경
                             Toast.makeText(context, "변경사항이 수정되었습니다.", Toast.LENGTH_LONG).show();
@@ -126,14 +207,64 @@ public class InfoFragment extends Fragment {
         });
 
 
-
-
-
-
-
         return root;
 
     }
+
+    public boolean ConfirmPassword(String input) {
+        boolean result = false;
+        //비밀번호 조건은 8자리~16자리
+        // 영문 대소문자, 숫자 혼합 두가지만 하면 true
+
+        Pattern pAlphaLow = Pattern.compile("[a-z]]");
+        Pattern pAlphaUp = Pattern.compile("[A-Z]]");
+        Pattern pNumber = Pattern.compile("[0-9]]");
+        Matcher match;
+        int nCharType = 0;//비밀번호가 몇가지의 조합인지를 확인
+
+        // 영소문자 포함?
+        match = pAlphaLow.matcher(input);
+        if (match.find())
+            nCharType++;
+        // 영대문자 포함?
+        match = pAlphaUp.matcher(input);
+        if (match.find())
+            nCharType++;
+        // 숫자 포함?
+        match = pNumber.matcher(input);
+        if (match.find())
+            nCharType++;
+
+        // 두가지 이상 섞여있고 길이 제한도 잘 지켜진 경우
+        //if( nCharType >=2 && 8 <= input.length() && input.length() <= 16 ){
+        if (8 <= input.length() && input.length() <= 16) {
+            result = true;
+        }
+        //else if( nCharType < 2 || input.length() < 8 || input.length() > 16 ){
+        else if (input.length() < 8 || input.length() > 16) {
+            //Toast.makeText(SignUpActivity.this, "nCharType:"+nCharType+" input.length():"+input.length(), Toast.LENGTH_SHORT).show();
+            result = false;
+        }
+        return result;
+    }
+
+
+
+    public boolean ConfirmPhonenum(String input){
+        boolean result = false;
+
+        String regExp_mobile ="^01[016789][.-]?\\d{3,4}[.-]?\\d{4}$";
+        String regExp_wireline = "^\\d{2,3}[.-]?\\d{3,4}[.-]?\\d{4}$";
+
+        if( input.matches(regExp_mobile) || input.matches(regExp_wireline) ){
+            result = true;
+        }else {
+            result = false;
+        }
+
+        return result;
+    }
+
 
     //가게의 정보를 가져옴
     private void getMyRestaurantInfo(@NonNull DataSnapshot dataSnapshot, String myId) {
