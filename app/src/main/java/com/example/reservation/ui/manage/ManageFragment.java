8ceb623df84000ac362bf8e1dcbcbd31ad7d4f19
mimi <alal39603819@gmail.com>
2019-11-28 02:26:34 +0900

    manage open_hour,minute나눔

--- app/src/main/java/com/example/reservation/ui/manage/ManageFragment.java
+++ app/src/main/java/com/example/reservation/ui/manage/ManageFragment.java
@@ -1,12 +1,17 @@
 package com.example.reservation.ui.manage;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,15 +27,23 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ManageFragment extends Fragment {
 
     private ManageViewModel shareViewModel;
     private String restaurant1 = "";
-    private String open="";
-    private String close="";
-
+    private String open_hour="";
+    private String open_minute="";
+    private String close_hour="";
+    private String close_minute="";
+    EditText editrestaurant_name;
+    TimePicker open_tp, close_tp;
+    private static final int TIME_PICKER_INTERVAL=30;
+    Button editcheck;
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         shareViewModel =
                 ViewModelProviders.of(this).get(ManageViewModel.class);
         View root = inflater.inflate(R.layout.fragment_manage, container, false);
@@ -54,10 +67,20 @@ public class ManageFragment extends Fragment {
         final String id1 = intent.getExtras().getString("id");
         final String key1 = intent.getExtras().getString("key");
 
+        editrestaurant_name = (EditText) root.findViewById(R.id.restaurant_name);
+        open_tp=(TimePicker)root.findViewById(R.id.timePicker_open);
+        close_tp=(TimePicker)root.findViewById(R.id.timePicker_close);
+        editcheck = (Button) root.findViewById(R.id.check);
+        setTimePickerInterval(open_tp);
+        setTimePickerInterval(close_tp);
+
         myRef1.addListenerForSingleValueEvent(new ValueEventListener() {//id값으로 정보를 찾음
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 getMyRestaurantManage(dataSnapshot, id1);
+                editrestaurant_name.setText(restaurant1);
+
+
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
@@ -72,22 +95,41 @@ public class ManageFragment extends Fragment {
         return root;
 
     }
+
+
     private void getMyRestaurantManage(@NonNull DataSnapshot dataSnapshot, String myId) {
         for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
             User user_each = childSnapshot.getValue(User.class);
             if (user_each.getId1().equals(myId)) {//intent로 받은 값이랑 반복문을 통해서 확인한 아이디 값이랑 같으면
                 restaurant1 = user_each.getRestaurant_name();//어플 사용하는 사장님의 가게 이름이다
-                open=user_each.getOpen();
-                close=user_each.getClose();
-
+                open_hour=user_each.getOpen_hour();
+                open_minute=user_each.getOpen_minute();
+                close_hour=user_each.getClose_hour();
+                close_minute=user_each.getClose_minute();
                 Log.i("가게이름", restaurant1);
-                Log.i("manage_open",open);
-                Log.i("manage_close",close);
+                Log.i("manage_open",open_hour);
+                Log.i("manage_close",close_hour);
                 //Toast.makeText(getContext(), id1+"\n"+key1+"\nrestaurant1: "+restaurant1,Toast.LENGTH_LONG).show();
                 break;
             } else {
                 continue;
             }
+        }
+    }
+    //타임피커 30분단위 설정
+    private void setTimePickerInterval(TimePicker timePicker) {
+        try {
+            NumberPicker minutePicker = (NumberPicker) timePicker.findViewById(Resources.getSystem().getIdentifier(
+                    "minute", "id", "android"));
+            minutePicker.setMinValue(0);
+            minutePicker.setMaxValue((60/ TIME_PICKER_INTERVAL) - 1);
+            List<String> displayedValues = new ArrayList<String>();
+            for (int i = 0; i < 60; i += TIME_PICKER_INTERVAL) {
+                displayedValues.add(String.format("%02d", i));
+            }
+            minutePicker.setDisplayedValues(displayedValues.toArray(new String[0]));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
